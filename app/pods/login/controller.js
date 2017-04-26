17824f3dbde9ec203d61b97d33f22a1a7aa944a4
Matt Mazzola <mattmazzola@live.com>
2017-04-25 22:29:21 -0700

    Remove extra login buttons until implemented

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -2,19 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
-  
+
   actions: {
     loginWithFacebook() {
       console.log('loginWithFacebook');
       this.get('session').authenticate('authenticator:torii', 'facebook');
-    },
-
-    loginWithTwitter() {
-      console.log('loginWithTwitter');
-    },
-
-    loginWithGoogle() {
-      console.log('loginWithGoogle');
     }
   }
 });
