4480a827d0266fc247ace5dadc2560b82a2714d7
Matt Mazzola <mattmazzola@live.com>
2017-04-25 21:12:32 -0700

    Update facebook login button to invoke facebook torii authenticator

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+  
   actions: {
     loginWithFacebook() {
       console.log('loginWithFacebook');
+      this.get('session').authenticate('authenticator:torii', 'facebook');
     },
 
     loginWithTwitter() {
