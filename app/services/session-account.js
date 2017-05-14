10143af06a5a1295b3cb0a625845f3b08e0bf38d
Matt Mazzola <mattmazzola@live.com>
2017-05-14 12:50:54 -0700

    Update session-account to save profile on start, and if conflict search profile

--- app/services/session-account.js
+++ app/services/session-account.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 const {
   inject,
@@ -7,8 +8,43 @@ const {
 
 export default Ember.Service.extend({
   session: inject.service('session'),
+  store: inject.service('store'),
 
   userAuthData: computed('session.data.authenticated', function() {
     return this.get('session.data.authenticated');
+  }),
+
+  user: computed('session.data.authenticated.id', function () {
+    const userAuthData = this.get('session.data.authenticated')
+    const store = this.get('store')
+
+    const userPromise = new Ember.RSVP.Promise(resolve => {
+      if (userAuthData.id) {
+        // Convert snake_case properties to cameCase properties
+        const camelizedUser = Object.keys(userAuthData).reduce((newObj, key) => {
+          const camelizedKey = Ember.String.camelize(key)
+          newObj[camelizedKey] = userAuthData[key]
+          return newObj
+        }, {})
+
+        // Set manual properties not automatically set by authData by 
+        camelizedUser.minAgeRange = userAuthData.age_range.min
+        camelizedUser.providerId = userAuthData.id
+
+        const newUser = store.createRecord('user', camelizedUser)
+        const newUserPromise = newUser.save()
+          .catch(error => {
+            return store.find('user', userAuthData.id)
+          })
+        resolve(newUserPromise)
+      }
+      else {
+        resolve(null)
+      }
+    })
+
+    return DS.PromiseObject.create({
+      promise: userPromise
+    })
   })
 });
