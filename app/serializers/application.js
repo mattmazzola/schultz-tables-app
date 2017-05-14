89f1e9fe5b98c9260dc1f27f7930aebbb341cf41
Matt Mazzola <mattmazzola@live.com>
2017-05-14 12:50:08 -0700

    Add application adapter and serializer using oauth2 authorizer

---
+++ app/serializers/application.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+  serializeIntoHash(hash, typeClass, snapshot, options) {
+    const newHash = this.serialize(snapshot, options);
+    Object.assign(hash, newHash)
+  },
+
+  normalizeSaveResponse(store, primaryModelClass, payload, id, requestType) {
+    let normalizedRootKey = this.payloadKeyFromModelName(primaryModelClass.modelName);
+
+    return this.normalizeSingleResponse(store, primaryModelClass, { [normalizedRootKey]: payload }, id, requestType);
+  },
+
+  normalizeFindRecordResponse(store, primaryModelClass, payload, id, requestType) {
+    let normalizedRootKey = this.payloadKeyFromModelName(primaryModelClass.modelName);
+    
+    return this.normalizeSingleResponse(store, primaryModelClass, { [normalizedRootKey]: payload }, id, requestType);
+  }
+});
