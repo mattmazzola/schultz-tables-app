dbbf30bca41a597c39e7dbf6c6603599d0f50e69
Matt Mazzola <mattmazzola@live.com>
2017-06-11 23:00:56 -0700

    Make all routes other than login authenticated to force redirect when not logged in

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 const {
   inject
 } = Ember;
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   tableService: inject.service('table'),
 
   actions: {
