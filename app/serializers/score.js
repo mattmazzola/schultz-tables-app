2c46a62b4f67b121321928dfaad6b1b74fbdd4bb
Matt Mazzola <mattmazzola@live.com>
2017-06-13 21:48:23 -0700

    Remove user as link and make it side loaded resource

--- app/serializers/score.js
+++ app/serializers/score.js
@@ -1,13 +1,17 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    return this._normalizeResponse(store, primaryModelClass, payload, id, requestType, false);
+  },
+  
   normalize(modelClass, resourceHash, prop) {
-    // embedded?
-    // resourceHash.user = { id: resourceHash.userId };
+    // Set user proprty to the userId to fetch side loaded model
+    resourceHash.user = resourceHash.userId;
 
+    // Set link to fetch record asynchronously
     resourceHash.links = {
-      user: `/api/users/${resourceHash.userId}`,
-      scoreDetails: `/api/scores/${resourceHash.scoreDetailsId}/details`
+      details: `/api/scores/${resourceHash.scoreDetailsId}/details`
     }
 
     return this._super(modelClass, resourceHash, prop);
