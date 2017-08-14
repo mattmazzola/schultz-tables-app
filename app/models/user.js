a1e3aeb2f59ab742fc1d5f2799d8a1c70d580929
Matt Mazzola <mattmazzola@live.com>
2017-08-13 21:51:29 -0700

    Add user scores as partial model.

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
+import { PartialModel, partial } from 'ember-data-partial-model/utils/model';
+const { attr } = DS;
 
-const {
-  attr
-} = DS
-
-export default DS.Model.extend({
+export default PartialModel.extend({
   email: attr('string'),
-  name: attr('string')
+  name: attr('string'),
+  extended: partial('user', 'extended', {
+    scores: attr()
+  })
 });
